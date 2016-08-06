d26b382db8ec747611ab30cf09657a599e64b24f
Brad Martin <bradwaynemartin@gmail.com>
2016-08-05 21:07:18 -0500

    fix definitions

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,39 @@
-export * from './pulltorefresh'
+/**
+ * Contains the PullToRefresh class, which represents a Layout that contains the UI pattern for pull-to-refresh
+ */
+
+import common = require("./pulltorefresh-common");
+export declare class PullToRefresh extends common.PullToRefresh {
+
+    /**
+     * String value used when hooking to the onRefresh event.
+     */
+    public static refreshEvent: string;
+
+    /**
+     * Gets the native [android widget](http://developer.android.com/reference/android/support/v4/widget/SwipeRefreshLayout.html) that represents the user interface for this component. Valid only when running on Android OS.
+     */
+    android: any /* android.support.v4.widget.SwipeRefreshLayout */;
+
+    /**
+     * Because of iOS specific this returns the basic UIView. In order to access UIRefreshControl use the refreshControl property!
+     */
+    ios: any
+
+    /**
+     * Returns the native iOS UIRefreshControl
+     */
+    refreshControl: UIRefreshControl
+
+    /*
+    * Gets or sets if the view is refreshing
+    */
+    refreshing: boolean;
+
+    /**
+     * Raised when a refresh event occurs.
+     */
+    on(event: string, callback: (args: observable.EventData) => void, thisArg?: any);
+    on(event: "refresh", callback: (args: observable.EventData) => void, thisArg?: any);
+
+}
