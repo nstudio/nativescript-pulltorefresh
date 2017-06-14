093767ba918f5694e0760d661f7d3a4081dffce3
Brad Martin <bradwaynemartin@gmail.com>
2017-06-14 12:04:31 -0500

    test prettier on def file

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,33 @@
-import { ContentView } from 'ui/content-view';
+import { ContentView } from "tns-core-modules/ui/content-view";
 
-export * from "ui/content-view";
+export * from "tns-core-modules/ui/content-view";
 
 /**
  * Contains the PullToRefresh class, which represents a Layout that contains the UI pattern for pull-to-refresh
  */
 export class PullToRefresh extends ContentView {
-
-    /**
+  /**
      * String value used when hooking to the onRefresh event.
      */
-    public static refreshEvent: string;
+  public static refreshEvent: string;
 
-    /**
+  /**
      * Gets the native [android widget](http://developer.android.com/reference/android/support/v4/widget/SwipeRefreshLayout.html) that represents the user interface for this component. Valid only when running on Android OS.
      */
-    android: any /* android.support.v4.widget.SwipeRefreshLayout */;
+  android: any /* android.support.v4.widget.SwipeRefreshLayout */;
 
-    /**
+  /**
      * Because of iOS specific this returns the basic UIView. In order to access UIRefreshControl use the refreshControl property!
      */
-    ios: any
+  ios: any;
 
-    /**
+  /**
      * Returns the native iOS UIRefreshControl
      */
-    refreshControl?: any /// UIRefreshControl
+  refreshControl?: any; /// UIRefreshControl
 
-    /*
+  /*
     * Gets or sets if the view is refreshing
     */
-    refreshing: boolean;
+  refreshing: boolean;
 }
