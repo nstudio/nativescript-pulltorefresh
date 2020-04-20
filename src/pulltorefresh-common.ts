5b2f41185a93fbaa4da3c8f5a937b3a767ef0134
DimitrisRK <55595100+DimitrisRK@users.noreply.github.com>
2020-04-20 04:58:09 +0300

    Android NPE fix.
    This overridden method prevents color from being inherited, therefore color is null and an NPE is thrown for android TextViews that have no color.
    Also, it seems to be useless as logic is handled by ContentView.

--- src/pulltorefresh-common.ts
+++ src/pulltorefresh-common.ts
@@ -9,20 +9,6 @@ export class PullToRefreshBase extends ContentView
   public static refreshEvent = 'refresh';
 
   public refreshing: boolean;
-
-  public _addChildFromBuilder(name: string, value: any) {
-    // copy inheritable style property values
-    const originalColor = value.style.color || null;
-    const originaBackgroundColor = value.style.backgroundColor || null;
-
-    if (value instanceof View) {
-      this.content = value;
-    }
-
-    // reset inheritable style property values as we do not want those to be inherited
-    value.style.color = originalColor;
-    value.style.backgroundColor = originaBackgroundColor;
-  }
 }
 
 export const refreshingProperty = new Property<PullToRefreshBase, boolean>({
